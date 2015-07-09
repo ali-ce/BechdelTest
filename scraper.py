6cd153aab2758973da81c1e6fa0facfb85d72b93
Alice <alice@silk.co>
2015-07-09 16:54:54 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,10 +6,7 @@ import requests
 
 html = requests.get("http://bechdeltest.com/?list=all").text
 root = lxml.html.fromstring(html)
-data = {
- 'MovieId' : [movieID.attrib['id'] for movieid in root.cssselect("div[class='movie'] a"),
- 'MovieTitle' : [title.text_content().encode('utf-8') for title in root.cssselect("div[class='movie'] a"),
- }
+data = {'MovieId': [movieID.attrib['id'] for movieid in root.cssselect("div[class='movie'] a"), 'Title': [title.text_content().encode('utf-8') for title in root.cssselect("div[class='movie'] a")}
 scraperwiki.sqlite.save(unique_keys = ['MovieId'], data=data)
 #for el in root.cssselect("div[class='movie'] a"):
  #title = el.text_content().encode('utf-8')
