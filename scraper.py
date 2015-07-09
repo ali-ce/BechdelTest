626769b703a179d4728950f81844905a943087aa
Alice <alice@silk.co>
2015-07-09 14:31:31 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,18 +1,23 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
+import scraperwiki
+import lxml.html
 #
 # # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
+html = scraperwiki.scrape("http://bechdeltest.com/?list=all")
 #
 # # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
+root = lxml.html.fromstring(html)
+title = root.cssselect("//div[@class='movie']/a[2]/text()")
+testsPassed = root.cssselect("//div[@class='movie']/a[1]/img/@alt")
+testComment = root.cssselect("//div[@class='movie']/a[2]/img/@title")
+bechdelUrl = root.cssselect("//div[@class='movie']/a[2]/@href")
+imdbUrl = root.cssselect("//div[@class='movie']/a[1]/@href)")
+movieId = root.cssselect("//div[@class='movie']/a[2]/@id)")
+
 # # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+scraperwiki.sqlite.save(unique_keys=['movieID'], data={"Movie Title" : title, "Tests Passed" : testsPassed, "Test Passed Details" : testComment, "Profile" : bechdelUrl, "IMDb Link" : imdbUrl})
 #
 # # An arbitrary query against the database
 # scraperwiki.sql.select("* from data where 'name'='peter'")
