9da36d9e9bb9c5f809022c618d8262ea9a0a4a83
Alice <alice@silk.co>
2015-07-10 13:07:14 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ import requests
 
 html = requests.get("http://bechdeltest.com/?list=all").text
 root = lxml.html.fromstring(html)
-movieList = [link.get('href')[0] for link in root.cssselect("div[class='movie'] a")]
+movieList = [link.get('href') for link in root.cssselect("div[class='movie'] a(1)")]
 print movieList
 
 #for movie in movieList:
