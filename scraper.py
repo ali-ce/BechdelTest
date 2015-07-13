16890ee5c723bfc6401c6bf248a1717ac39eda02
Alice <alice@silk.co>
2015-07-13 17:50:23 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import scraperwiki
 import lxml.html
 import requests
+import json
 
 html = requests.get("http://bechdeltest.com/?list=all").text
 root = lxml.html.fromstring(html)
@@ -49,52 +50,3 @@ for link in movieList:
  omdbUrl = "http://www.omdbapi.com/?i="+imdbID
 #Omdb Data
 #Write to Database
- 
-
-
-#Dubious?
-
-#for movie in movieList:
-# movieID = movie.attrib['id']
-# title = movie.text_content().encode('utf-8')
- #imdbUrl = movie.attrib['href'] [0]
- #passed = movieList.cssselect( "img").attrib['alt']
- #passInfo = movieList.cssselect( "img").attrib['title']
- #link = movie.attrib['href'] [1]
- #data = {
-  #'movieID': movieID,
-  #'Title': title,
-  #'IMDb Profile': imdbUrl,
-  #'Number of Criteria Passed': passed,
-  #'Details on Criteria Passed': passInfo,
-  #'Source and Discussion': link
-  #}
- #scraperwiki.sqlite.save(unique_keys = ['movieID'], data=data)
- #print data
-
-#for el in root.cssselect("div[class='movie'] a"):
- #title = el.text_content().encode('utf-8')
- #print title
-#data = {
- # 'Title' : [title.text_content() for title in root.cssselect("div[class= 'movie'] a ")][2]
-  
-#scraperwiki.sqlite.save(unique_keys = ['Product'], data=data)
-
-#for el in root.cssselect("div[class='movie'] a"):
-#testsPassed = root.cssselect("//div[@class='movie']/a[1]/img/@alt")
-#testComment = root.cssselect("//div[@class='movie']/a[2]/img/@title")
-#bechdelUrl = root.cssselect("//div[@class='movie']/a[2]/@href")
-#imdbUrl = root.cssselect("//div[@class='movie']/a[1]/@href)")
-#movieId = root.cssselect("//div[@class='movie']/a[2]/@id)")
-
-# # Write out to the sqlite database using scraperwiki library
-#scraperwiki.sqlite.save(unique_keys=['movieID'], data={"Movie Title" : title, "Tests Passed" : testsPassed, "Test Passed Details" : testComment, "Profile" : bechdelUrl, "IMDb Link" : imdbUrl})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
