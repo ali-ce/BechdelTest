bfc718f8254859d7ad0583dced0c03199fb52049
Alice <alice@silk.co>
2015-07-13 14:48:05 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,7 +12,7 @@ for link in movieList:
  movieID = link.split("/")[4]
  title = rootMovie.cssselect("h2 a:nth-of-type(1)")[0].text_content()
  year = title.split(' ')[-1].replace('(','').replace(')','')
- imdbUrl = rootMovie.cssselect("h2 a")
+ imdbUrl = rootMovie.cssselect("h2 a")[0].attrib('href')
  print imdbUrl
  break
  
