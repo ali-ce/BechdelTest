58752b8904bab538e9740c42139655aabc11abac
Alice <alice@silk.co>
2015-07-13 12:35:32 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,7 +10,7 @@ for link in movieList:
  htmlMovie = requests.get(link).text
  rootMovie = lxml.html.fromstring(htmlMovie)
  movieID = link.split("/")[4]
- title = rootMovie.cssselect("h2 a:nth-of-type(1)")[1].text_content()
+ title = rootMovie.cssselect("h2 a:nth-of-type(1)")
  print title
  break
  #movieTitle = 
