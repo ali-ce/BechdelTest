8957ce69dbe58a79da362cb682d1bc36246eb572
Alice <alice@silk.co>
2015-07-14 10:36:14 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,6 +7,7 @@ import urllib
 html = requests.get("http://bechdeltest.com/?list=all").text
 root = lxml.html.fromstring(html)
 movieList = ["http://bechdeltest.com"+link.get('href') for link in root.cssselect("div[class='movie'] a:nth-of-type(3)")]
+print movieList
 rows_scraped = 0
 number_movies = len(movieList)
 for link in movieList:
